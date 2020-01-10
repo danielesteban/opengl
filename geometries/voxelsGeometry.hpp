598d04b221a650d2cd0fe19bc2077f649461074c
Daniel Esteban Nombela <dani@gatunes.com>
2020-01-10 06:24:02 +0100

    Typo

--- geometries/voxelsGeometry.hpp
+++ geometries/voxelsGeometry.hpp
@@ -7,5 +7,5 @@ class VoxelsGeometry : public Geometry {
   public:
     VoxelsGeometry(Chunks *chunks, const GLint x, const GLint y, const GLint z);
   private:
-    static const GLfloat ao(const bool m1, const bool n2, const bool n3);
+    static const GLfloat ao(const bool n1, const bool n2, const bool n3);
 };
